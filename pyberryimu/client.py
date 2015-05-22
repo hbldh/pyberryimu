c8cb486dd9f1ec97c919d97ded10a1032b55ef61
Henrik Blidh <henrik.blidh@nedomkull.com>
2015-05-22 13:30:41 +0200

    Version 0.1.3
    Refactored intermal methods.
    Some exception handling added.

--- pyberryimu/client.py
+++ pyberryimu/client.py
@@ -21,6 +21,7 @@ from __future__ import absolute_import
 
 import math
 import time
+import datetime
 
 from smbus import SMBus
 
@@ -29,7 +30,16 @@ from pyberryimu.sensors import LSM9DS0, BMP180
 
 
 class BerryIMUClient(object):
-    """"""
+    """Client for using the BerryIMU from Python.
+
+    Datasheets for BerryIMU::
+
+        * `LSM9DS0 reference sheet
+            <http://ozzmaker.com/wp-content/uploads/2014/12/LSM9DS0.pdf>`_
+        * `BMP180 reference sheet
+            <http://ozzmaker.com/wp-content/uploads/2015/01/BMP180-DS000-09.pdf>`_
+
+    """
 
     def __init__(self, bus=1, acc_setup=1, gyro_setup=1, mag_setup=1, raw_output=False):
         """Constructor for BerryIMUClient"""
@@ -43,7 +53,18 @@ class BerryIMUClient(object):
         self._mag_setup = mag_setup
         self._raw_output = raw_output
 
-        self._bmp180_calibration = None
+        # BMP180 calibration values.
+        self.__ac1 = None
+        self.__ac2 = None
+        self.__ac3 = None
+        self.__ac4 = None
+        self.__ac5 = None
+        self.__ac6 = None
+        self.__b1 = None
+        self.__b2 = None
+        self.__mb = None
+        self.__mc = None
+        self.__md = None
 
     @property
     def bus(self):
@@ -57,8 +78,14 @@ class BerryIMUClient(object):
         try:
             self._bus = SMBus(self._bus_no)
         except IOError as e:
-            # TODO: Handle these undocumented errors better.
-            raise
+            if str(e) == '2':
+                raise PyBerryIMUError("/dev/i2c-{0} not found.".format(self._bus_no))
+            elif str(e) == '13':
+                raise PyBerryIMUError("Permission to read and/or write to /dev/i2c-{0} missing.".format(self._bus_no))
+            else:
+                raise PyBerryIMUError('Unhandled IOError: {0}'.format(e))
+        except Exception as e:
+            raise PyBerryIMUError('Unhandled {0}: {1}'.format(type(e), e))
         else:
             self._init_accelerometer()
             self._init_gyroscope()
@@ -87,9 +114,9 @@ class BerryIMUClient(object):
         # TODO: Better init handling!
         if self._acc_setup == 1:
             # z,y,x axis enabled, continuous update, 100Hz data rate
-            self._write_to_accelerometer(LSM9DS0.CTRL_REG1_XM, 0b01100111)
+            self._write(LSM9DS0.ACC_ADDRESS, LSM9DS0.CTRL_REG1_XM, 0b01100111)
             # +/- 16G full scale
-            self._write_to_accelerometer(LSM9DS0.CTRL_REG2_XM, 0b00100000)
+            self._write(LSM9DS0.ACC_ADDRESS, LSM9DS0.CTRL_REG2_XM, 0b00100000)
         else:
             raise PyBerryIMUError("Invalid Accelerometer setup flag: {0}.".format(self._acc_setup))
 
@@ -98,9 +125,9 @@ class BerryIMUClient(object):
         # TODO: Better init handling!
         if self._gyro_setup == 1:
             # Normal power mode, all axes enabled
-            self._write_to_gyroscope(LSM9DS0.CTRL_REG1_G, 0b00001111)
+            self._write(LSM9DS0.GYR_ADDRESS, LSM9DS0.CTRL_REG1_G, 0b00001111)
             # Continuous update, 2000 dps full scale
-            self._write_to_gyroscope(LSM9DS0.CTRL_REG4_G, 0b00110000)
+            self._write(LSM9DS0.GYR_ADDRESS, LSM9DS0.CTRL_REG4_G, 0b00110000)
 
             # Conversion constants for this setting [deg/s/LSB]
             # TODO: Wrap G_GAIN in automatic calculation of value.
@@ -118,20 +145,19 @@ class BerryIMUClient(object):
         # TODO: Better init handling!
         if self._mag_setup == 1:
             # Temp enable, M data rate = 50Hz
-            self._write_to_magnetometer(LSM9DS0.CTRL_REG5_XM, 0b11110000)
+            self._write(LSM9DS0.MAG_ADDRESS, LSM9DS0.CTRL_REG5_XM, 0b11110000)
             # +/-12gauss
-            self._write_to_magnetometer(LSM9DS0.CTRL_REG6_XM, 0b01100000)
+            self._write(LSM9DS0.MAG_ADDRESS, LSM9DS0.CTRL_REG6_XM, 0b01100000)
             # Continuous-conversion mode
-            self._write_to_magnetometer(LSM9DS0.CTRL_REG7_XM, 0b00000000)
+            self._write(LSM9DS0.MAG_ADDRESS, LSM9DS0.CTRL_REG7_XM, 0b00000000)
         else:
             raise PyBerryIMUError("Invalid Magnetometer setup flag: {0}.".format(self._mag_setup))
 
     def _init_barometric_pressure_sensor(self):
         """Initialize the Barometric Pressure Sensor."""
-        # Read whole calibration EEPROM data
-        self._bmp180_calibration = self.bus.read_i2c_block_data(BMP180.ADDRESS, BMP180.CALIB_DATA_REG, 22)
+        self._set_bmp180_calibration_values()
+        # TODO: Better init handling!
         self.__OVERSAMPLING = 3  # 0..3
-        # TODO: Translate calibration bytes to data.
 
     def get_bmp180_chip_id_and_version(self):
         """Gets Chip ID and version for the BMP180 sensor.
@@ -142,18 +168,32 @@ class BerryIMUClient(object):
         """
         return self.bus.read_i2c_block_data(BMP180.ADDRESS, BMP180.CHIP_ID_REG, 2)
 
+    def _set_bmp180_calibration_values(self):
+        """Read, parse and store calibration EEPROM data to attributes."""
+
+        bmp180_calibration = self.bus.read_i2c_block_data(BMP180.ADDRESS, BMP180.CALIB_DATA_REG, 22)
+        vals = [msb + lsb for msb, lsb in zip(map(lambda x: x << 8, bmp180_calibration[::2]),
+                                              bmp180_calibration[1::2])]
+        for i in [0, 1, 2, 6, 7, 8, 9, 10]:
+            if vals[i] > 2 ** 15 - 1:
+                vals[i] -= 2 ** 16
+
+        self.__ac1 = vals[0]
+        self.__ac2 = vals[1]
+        self.__ac3 = vals[2]
+        self.__ac4 = vals[3]
+        self.__ac5 = vals[4]
+        self.__ac6 = vals[5]
+        self.__b1 = vals[6]
+        self.__b2 = vals[7]
+        self.__mb = vals[8]
+        self.__mc = vals[9]
+        self.__md = vals[10]
+
     # Methods for writing to BerryIMU.
 
-    def _write_to_accelerometer(self, register, value):
-        self.bus.write_byte_data(LSM9DS0.ACC_ADDRESS, register, value)
-        return -1
-
-    def _write_to_gyroscope(self, register, value):
-        self.bus.write_byte_data(LSM9DS0.GYR_ADDRESS, register, value)
-        return -1
-
-    def _write_to_magnetometer(self, register, value):
-        self.bus.write_byte_data(LSM9DS0.MAG_ADDRESS, register, value)
+    def _write(self, address, register, value):
+        self.bus.write_byte_data(address, register, value)
         return -1
 
     # Methods for reading from BerryIMU.
@@ -164,8 +204,15 @@ class BerryIMUClient(object):
 
         return value if value < 32768 else value - 65536
 
-    # TODO: Add conversion to proper units in reading methods.
-    # TODO: Add timestamp as output for all read methods?
+    @property
+    def timestamp(self):
+        """Timestamp right now in UTC Epoch time.
+
+        :return: UTC Epoch timestamp
+        :rtype: float
+
+        """
+        return time.mktime(datetime.datetime.utcnow().timetuple())
 
     def read_accelerometer(self):
         """Method for reading values from the accelerometer.
@@ -185,17 +232,6 @@ class BerryIMUClient(object):
         :rtype: tuple
 
         """
-        # # Convert Gyro raw to degrees per second
-        # rate_gyr_x = g_x * G_GAIN
-        # rate_gyr_y = g_y * G_GAIN
-        # rate_gyr_z = g_z * G_GAIN
-        #
-        #
-        # #Calculate the angles from the gyro. LP = loop period
-        # gyroXangle += rate_gyr_x * LP;
-        # gyroYangle += rate_gyr_y * LP;
-        # gyroZangle += rate_gyr_z * LP;
-
         return (self._read(LSM9DS0.GYR_ADDRESS, LSM9DS0.OUT_X_L_G, LSM9DS0.OUT_X_H_G),
                 self._read(LSM9DS0.GYR_ADDRESS, LSM9DS0.OUT_Y_L_G, LSM9DS0.OUT_Y_H_G),
                 self._read(LSM9DS0.GYR_ADDRESS, LSM9DS0.OUT_Z_L_G, LSM9DS0.OUT_Z_H_G))
@@ -214,69 +250,65 @@ class BerryIMUClient(object):
     def read_temperature(self):
         """Method for reading temperature values from the barometric pressure sensor.
 
+        Reference for reading to temperature calculations:
+        `BMP180 reference sheet
+        <http://ozzmaker.com/wp-content/uploads/2015/01/BMP180-DS000-09.pdf>`_
+
         :return: The temperature value.
         :rtype: int
 
         """
-        # TODO: Clean up calculation of temperature and pressure.
-        self.bus.write_byte_data(BMP180.ADDRESS, BMP180.WRITE_REG, 0x2E)
+        self._write(BMP180.ADDRESS, BMP180.WRITE_REG, 0x2E)
         time.sleep(0.005)
         msb, lsb = self.bus.read_i2c_block_data(BMP180.ADDRESS, BMP180.READ_REG, 2)
         ut = (msb << 8) + lsb
-    
-        ac1, ac2, ac3, ac4, ac5, ac6, b1, b2, mb, mc, md = self._get_bmp180_calib_values()
-        x1 = ((ut - ac6) * ac5) >> 15
-        x2 = (mc << 11) // (x1 + md)
+
+        x1 = ((ut - self.__ac6) * self.__ac5) >> 15
+        x2 = (self.__mc << 11) // (x1 + self.__md)
         b5 = x1 + x2
         t = (b5 + 8) >> 4
-        return ut, t / 10.0
+        return t / 10.0
 
     def read_pressure(self):
         """Method for reading pressure value from the barometric pressure sensor.
+
+        Reference for reading to pressure calculations:
+        `BMP180 reference sheet
+        <http://ozzmaker.com/wp-content/uploads/2015/01/BMP180-DS000-09.pdf>`_
+
 
         :return: The pressure value.
         :rtype: int
 
         """
-        # TODO: Clean up calculation of temperature and pressure.
-
-        ac1, ac2, ac3, ac4, ac5, ac6, b1, b2, mb, mc, md = self._get_bmp180_calib_values()
-
-        self.bus.write_byte_data(BMP180.ADDRESS, BMP180.WRITE_REG, 0x2E)
+        self._write(BMP180.ADDRESS, BMP180.WRITE_REG, 0x2E)
         time.sleep(0.005)
         msb, lsb = self.bus.read_i2c_block_data(BMP180.ADDRESS, BMP180.READ_REG, 2)
-
         ut = (msb << 8) + lsb
 
-        x1 = ((ut - ac6) * ac5) >> 15
-        x2 = (mc << 11) // (x1 + md)
-        b5 = x1 + x2
-        t = (b5 + 8) >> 4
-
-        self.bus.write_byte_data(BMP180.ADDRESS, BMP180.WRITE_REG, 0x34 + (self.__OVERSAMPLING << 6))
+        self._write(BMP180.ADDRESS, BMP180.WRITE_REG, 0x34 + (self.__OVERSAMPLING << 6))
         time.sleep(0.04)
         msb, lsb, xsb = self.bus.read_i2c_block_data(BMP180.ADDRESS, BMP180.READ_REG, 3)
         up = ((msb << 16) + (lsb << 8) + xsb) >> (8 - self.__OVERSAMPLING)
 
-        x1 = ((ut - ac6) * ac5) >> 15
-        x2 = (mc << 11) // (x1 + md)
+        x1 = ((ut - self.__ac6) * self.__ac5) >> 15
+        x2 = (self.__mc << 11) // (x1 + self.__md)
         b5 = x1 + x2
 
         b6 = b5 - 4000
         b62 = b6 * b6 >> 12
-        x1 = (b2 * b62) >> 11
-        x2 = ac2 * b6 >> 11
+        x1 = (self.__b2 * b62) >> 11
+        x2 = self.__ac2 * b6 >> 11
         x3 = x1 + x2
-        b3 = (((ac1 * 4 + x3) << self.__OVERSAMPLING) + 2) >> 2
+        b3 = (((self.__ac1 * 4 + x3) << self.__OVERSAMPLING) + 2) >> 2
 
-        x1 = ac3 * b6 >> 13
-        x2 = (b1 * b62) >> 16
+        x1 = self.__ac3 * b6 >> 13
+        x2 = (self.__b1 * b62) >> 16
         x3 = ((x1 + x2) + 2) >> 2
-        b4 = (ac4 * (x3 + 32768)) >> 15
+        b4 = (self.__ac4 * (x3 + 32768)) >> 15
         b7 = (up - b3) * (50000 >> self.__OVERSAMPLING)
 
         p = (b7 * 2) // b4
-        # p = (b7 / b4) * 2
 
         x1 = (p >> 8) * (p >> 8)
         x1 = (x1 * 3038) >> 16
@@ -284,11 +316,3 @@ class BerryIMUClient(object):
         p += (x1 + x2 + 3791) >> 4
 
         return p / 100.0
-
-    def _get_bmp180_calib_values(self):
-        vals = [msb + lsb for msb, lsb in zip(map(lambda x: x << 8, self._bmp180_calibration[::2]),
-                                              self._bmp180_calibration[1::2])]
-        for i in [0, 1, 2, 6, 7, 8, 9, 10]:
-            if vals[i] > 2**15 - 1:
-                vals[i] -= 2**16
-        return vals
